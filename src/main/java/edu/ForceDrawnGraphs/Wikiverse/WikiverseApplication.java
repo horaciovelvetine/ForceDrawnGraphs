1d810389b3a5ea974149842af29be5c51993deae
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-11-16 22:18:59 -0700

    init new project with Spring init.io

---
+++ src/main/java/edu/ForceDrawnGraphs/Wikiverse/WikiverseApplication.java
@@ -0,0 +1,13 @@
+package edu.ForceDrawnGraphs.Wikiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ForceDrawnGraphs/Wikiverse/WikiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ForceDrawnGraphs.Wikiverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
