9de12e19b75fae77986e39950b41b768c39575fb
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-11-17 21:54:57 -0700

    init structure for preffered simple logging and helpful console display utils

---
+++ src/main/java/edu/ForceDrawnGraphs/Wikiverse/utils/Loggable.java
@@ -0,0 +1,37 @@
+package edu.ForceDrawnGraphs.Wikiverse.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public interface Loggable {
+  public static final String LOG_FILE_PATH = "src/main/resources/logs";
+
+  public default void log(Exception e) {
+    log(e.getMessage());
+  }
+
+  public default void log(String message) {
+    log(message, "debug_log.log");
+  }
+
+  public default void log(String message, String logFileName) {
+    try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE_PATH + logFileName, true))) {
+      writer.println(getTimeStamp() + "\n" + message);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public default String getTimeStamp() {
+    LocalDateTime now = LocalDateTime.now();
+    return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+  }
+
+  public default void print(String msg) {
+    System.out.println(msg);
+  }
+
+}
