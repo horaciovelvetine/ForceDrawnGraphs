7d973e69ea42ee44889790433512d7978231bdff
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-12-09 23:11:05 -0700

    haircut data path gibberish

--- src/main/java/edu/ForceDrawnGraphs/Wikiverse/db/LocalDatabase.java
+++ src/main/java/edu/ForceDrawnGraphs/Wikiverse/db/LocalDatabase.java
@@ -18,15 +18,6 @@ import edu.ForceDrawnGraphs.Wikiverse.utils.Loggable;
 
 @Component
 public class LocalDatabase implements Loggable {
-  // Local data files...
-  public static final String RESOURCE_PATH = "src/main/resources/";
-  public static final String ITEM_ALIASES_PATH = RESOURCE_PATH + "data/item_aliases.csv";
-  public static final String ITEM_PATH = RESOURCE_PATH + "data/item.csv";
-  public static final String LINK_ANNOTATED_TEXT_PATH = RESOURCE_PATH + "data/link_annotated_text.jsonl";
-  public static final String PAGE_PATH = RESOURCE_PATH + "data/page.csv";
-  public static final String PROPERTY_ALIASES_PATH = RESOURCE_PATH + "data/property_aliases.csv";
-  public static final String PROPERTY_PATH = RESOURCE_PATH + "data/property.csv";
-  public static final String STATEMENTS_PATH = RESOURCE_PATH + "data/statements.csv";
   // DB Config params
   public static final String DB_NAME = "en_Wikipedia_page_text_12_01_2019"; // ==> MATCHES LOCAL PG DB INSTANCE
   private static final String USER = "postgres";
@@ -79,14 +70,5 @@ public class LocalDatabase implements Loggable {
     } catch (IOException e) {
       log(e);
     }
-  }
-
-  @Override
-  public String toString() {
-    return "LocalDatabase [ " + ", ITEM_ALIASES_PATH=" + ITEM_ALIASES_PATH
-        + ", ITEM_PATH=" + ITEM_PATH + ", LINK_ANNOTATED_TEXT_PATH=" + LINK_ANNOTATED_TEXT_PATH
-        + ", PAGE_PATH=" + PAGE_PATH + ", PROPERTY_ALIASES_PATH=" + PROPERTY_ALIASES_PATH
-        + ", PROPERTY_PATH=" + PROPERTY_PATH + ", STATEMENTS_PATH=" + STATEMENTS_PATH + ", DB_NAME="
-        + DB_NAME + ", USER=" + USER + ", PASS=" + PASS + ", URL=" + URL + "]";
   }
 }
