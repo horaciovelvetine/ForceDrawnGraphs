a14312958dc6309a9eb890077221c7110eead6b0
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-02-11 21:19:18 -0700

    add jdbc for query execution

--- src/main/java/edu/ForceDrawnGraphs/util/ExecuteSQL.java
+++ src/main/java/edu/ForceDrawnGraphs/util/ExecuteSQL.java
@@ -4,17 +4,24 @@ import java.io.IOException;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
   * Execute the SQL file at the given resource path.
   * 
   * @param sqlFileResourcePath the resource path of the SQL file
   * 
+  * @param jdbc the JdbcTemplate to execute the SQL statements
+  * 
   * @implSpec The SQL file must be located in the resources directory.
   */
 public interface ExecuteSQL {
 
-  public default void executeSQL(String sqlFileResourcePath) throws IOException {
+  public default void executeSQL(String sqlFileResourcePath, JdbcTemplate jdbc)
+      throws IOException, IllegalArgumentException {
+    if (sqlFileResourcePath == null || sqlFileResourcePath.isBlank()) {
+      throw new IllegalArgumentException("sqlFileResourcePath cannot be null or blank");
+    }
     Resource resource = new ClassPathResource(sqlFileResourcePath);
     byte[] sqlFileBytes = resource.getInputStream().readAllBytes();
     String sql = new String(sqlFileBytes);
@@ -23,8 +30,7 @@ public interface ExecuteSQL {
     String[] sqlStatements = sql.split(";\\s*\\r?\\n");
     for (String statement : sqlStatements) {
       if (statement.trim().length() > 0) {
-        // Execute the SQL statement
-        // ...
+        jdbc.execute(statement);
       }
     }
   }
