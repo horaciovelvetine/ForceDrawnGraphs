0bdfd3cbea4ee4eb44e5334425540b7264af441c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-02-18 12:19:47 -0700

    Add a reset command to make testing either - drops and creates tables

---
+++ src/main/java/edu/ForceDrawnGraphs/commands/ResetLocalSet.java
@@ -0,0 +1,43 @@
+package edu.ForceDrawnGraphs.commands;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import edu.ForceDrawnGraphs.util.ExecuteSQL;
+import edu.ForceDrawnGraphs.util.Reportable;
+
+@ShellComponent
+public class ResetLocalSet implements Reportable, ExecuteSQL {
+  private JdbcTemplate jdbcTemplate;
+
+  public ResetLocalSet(DataSource dataSource) {
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
+  }
+
+  @ShellMethod("Drop tables and recreate the local set schema.")
+  public void reset() {
+    report("Begin 'reset', looking for existing data...");
+    dropLocalSetSchema();
+    createLocalSetSchema();
+    report("End 'reset', local set schema reset.");
+  }
+
+  private void dropLocalSetSchema() {
+    try {
+      executeSQL("DropLocalSetSchema.sql", jdbcTemplate);
+    } catch (Exception e) {
+      report(e);
+    }
+  }
+
+  private void createLocalSetSchema() {
+    try {
+      executeSQL("CreateLocalSetSchema.sql", jdbcTemplate);
+    } catch (Exception e) {
+      report(e);
+    }
+  }
+}
