1e18f8968f8eb78a620463d4c447a81369ba52e6
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-02-21 20:37:40 -0700

    finalizes checking for number of files

--- src/main/java/edu/ForceDrawnGraphs/commands/FindAndUpdateRecordCounts.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/FindAndUpdateRecordCounts.java
@@ -3,6 +3,7 @@ package edu.ForceDrawnGraphs.commands;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -10,36 +11,53 @@ import edu.ForceDrawnGraphs.models.LocalSetInfo;
 import edu.ForceDrawnGraphs.util.FindTotalRecordsInFile;
 import edu.ForceDrawnGraphs.util.Reportable;
 
+/**
+ * This class is responsible for finding and updating the record counts in each file of the data set.
+ */
 @ShellComponent
 public class FindAndUpdateRecordCounts implements FindTotalRecordsInFile, Reportable {
   private JdbcTemplate jbdcTemplate;
-  private LocalSetInfo localSetInfo;
+  private LocalSetInfo localSetInfo = new LocalSetInfo();
 
+  /**
+   * Constructs a new FindAndUpdateRecordCounts object with the specified data source.
+   *
+   * @param dataSource the data source used for database operations
+   */
+  @SuppressWarnings("null")
   public FindAndUpdateRecordCounts(DataSource dataSource) {
     this.jbdcTemplate = new JdbcTemplate(dataSource);
-    //? If this looks up the LocalSetInfo obj on init on bean startup is that potentially an issue? 
   }
 
-  @ShellMethod("Count and update the number of records in each file of the data set.")
-  public void counts() {
-    /*
-     * 1) Find LocalSetInfo if exists
-     * 2a) If exists call findNumberOfRecordsInFile on each file (on LocalSetInfo object)
-     * 2b) If not exists, create new LocalSetInfo object, see 2a
-     * 3) Update the LocalSetInfo object in the db with the new values for each file.
-     */
-
-    //2a & 3
-    updateLocalSetInfo();
-  }
-
+  /**
+   * Counts and updates the number of records in each file of the data set.
+   */
   @SuppressWarnings("null")
-  private void updateLocalSetInfo() {
+  @ShellMethod("Count and report the number of records in each file of the data set.")
+  public void counts() {
+    findLocalSetInfo();
     localSetInfo.findRecordTotals();
     try {
       jbdcTemplate.update(localSetInfo.getSQLUpdateQuery());
     } catch (Exception e) {
       report(e);
     }
+    report(localSetInfo.toString());
   }
+
+  /**
+   * Get the info regarding state of import of local set, and update the local set info object. 
+   */
+  private void findLocalSetInfo() {
+    String sql = "SELECT * FROM local_set_info WHERE id = 1;";
+    try {
+      SqlRowSet results = jbdcTemplate.queryForRowSet(sql);
+      if (results.next()) {
+        localSetInfo.mapRowResultsToLocalSetInfo(results);
+      }
+    } catch (Exception e) {
+      report(e);
+    }
+  }
+
 }
