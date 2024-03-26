4123fe3e3663c6764310bbc4c59a345d3afab6b4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-03-25 23:23:27 -0600

    adds responsibilty to existing model

--- src/main/java/edu/ForceDrawnGraphs/models/LinkAnnotatedTextRecord.java
+++ src/main/java/edu/ForceDrawnGraphs/models/LinkAnnotatedTextRecord.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class LinkAnnotatedTextRecord {
+import edu.ForceDrawnGraphs.interfaces.Reportable;
+
+public class LinkAnnotatedTextRecord implements Reportable {
 
   @JsonAlias("page_id")
   public final int pageId;
@@ -19,5 +22,16 @@ public class LinkAnnotatedTextRecord {
   public LinkAnnotatedTextRecord(int pageId, List<SectionRecord> sections) {
     this.pageId = pageId;
     this.sections = sections;
+  }
+
+  public LinkAnnotatedTextRecord createLATRFromStringData(String stringifiedJSON) {
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      return mapper.readValue(stringifiedJSON, LinkAnnotatedTextRecord.class);
+
+    } catch (Exception e) {
+      report(e);
+    }
+    return null;
   }
 }
