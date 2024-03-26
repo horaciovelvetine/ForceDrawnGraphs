4bd34c77e703295c375088a313118bdf288c7a33
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-03-25 23:23:41 -0600

    outlines initial object design

---
+++ src/main/java/edu/ForceDrawnGraphs/models/Edge.java
@@ -0,0 +1,10 @@
+package edu.ForceDrawnGraphs.models;
+
+public class Edge {
+  private int id;
+  private int srcVertexId;
+  private int tgtVertextId;
+  private int weight;
+  private int edgeTypeId;
+  private String edgeType;
+}
