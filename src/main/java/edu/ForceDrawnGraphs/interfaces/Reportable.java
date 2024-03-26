5065ebd0b54e5555c21f92bc8e073cf9253b90a2
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-03-25 23:20:08 -0600

    adds interfaces for widely inherited/base application use

---
+++ src/main/java/edu/ForceDrawnGraphs/interfaces/Reportable.java
@@ -0,0 +1,122 @@
+package edu.ForceDrawnGraphs.interfaces;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The Reportable interface provides methods for logging and reporting messages and exceptions.
+ */
+public interface Reportable {
+
+  /**
+   * The path to the log files.
+   */
+  public static final String LOG_FILE_PATH = "src/main/resources/logs/";
+
+  /**
+   * Returns the current timestamp in the format "yyyy-MM-dd HH:mm:ss".
+   *
+   * @return the current timestamp
+   */
+  public default String getTimeStamp() {
+    LocalDateTime now = LocalDateTime.now();
+    return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+  }
+
+  /**
+   * Logs exception.
+   *
+   * @param e the exception to be logged
+   */
+  public default void log(Exception e) {
+    log(e.getMessage());
+  }
+
+  /**
+   * Logs message.
+   *
+   * @param message the message to be logged
+   */
+  public default void log(String message) {
+    log(message, "debug.log");
+  }
+
+  /**
+   * Logs message to the specified log file.
+   *
+   * @param message     the message to be logged
+   * @param logFileName the name of the log file
+   */
+  public default void log(String message, String logFileName) {
+    print("Logged@ " + LOG_FILE_PATH + logFileName);
+    try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE_PATH + logFileName, true))) {
+      writer.println("\n" + getTimeStamp() + "\n" + message);
+    } catch (IOException e) {
+      print("\n" + e.getMessage() + "\n" + "Error in logging process: " + "\n" + message);
+    }
+  }
+
+  /**
+   * Prints message to the console.
+   *
+   * @param msg the message to be printed
+   */
+  public default void print(String msg) {
+    System.out.println(msg + "\n");
+  }
+
+  /**
+   * Prints exception to the console.
+   *
+   * @param e the exception to be printed
+   */
+  public default void print(Exception e) {
+    print(e.getMessage());
+  }
+
+  /**
+   * Reports message by printing it to the console and logging it.
+   *
+   * @param msg the message to be reported
+   */
+  public default void report(String msg) {
+    print(msg);
+    log(msg);
+  }
+
+  /**
+   * Reports exception by printing it to the console and logging it.
+   *
+   * @param e the exception to be reported
+   */
+  public default void report(Exception e) {
+    print(e);
+    log(e);
+  }
+
+  /**
+   * Reports message by printing it to the console and logging it to the specified log file.
+   *
+   * @param msg         the message to be reported
+   * @param logFileName the name of the log file
+   */
+  public default void report(String msg, String logFileName) {
+    print(msg);
+    log(msg, logFileName);
+  }
+
+  /**
+   * Reports message by printing it to the console and logging it to the specified log file.
+   *
+   * @param originMsg   Message placed before the exception message to include more context
+   * @param e           the exception to be reported
+   */
+  public default void report(String originMsg, Exception e) {
+    String msg = originMsg + "\n" + e.getMessage();
+    report(msg);
+  }
+
+}
