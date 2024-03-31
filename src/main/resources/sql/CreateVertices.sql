07000680f29fbaedfaa92e88d599b1804cc2c73c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-03-31 12:28:04 -0600

    continuin sql tweaks

--- src/main/resources/sql/CreateVertices.sql
+++ src/main/resources/sql/CreateVertices.sql
@@ -6,9 +6,8 @@ CREATE TABLE vertices (
     y FLOAT,
     z FLOAT,
     label VARCHAR(255),
-    en_label VARCHAR(255),
     en_description TEXT,
-    views int,
+    views VARCHAR(255),
     src_item_id VARCHAR(255),
     src_page_id VARCHAR(255)
 );
