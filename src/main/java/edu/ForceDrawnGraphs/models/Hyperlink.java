63bf770bbc9efb5ca0f678058bc0182aef228e9f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-01 12:26:36 -0600

    adds basedatarecordset models and updates for hyperlink and statement to grab relevant results and objectivize them on querey, onto next steps

--- src/main/java/edu/ForceDrawnGraphs/models/Hyperlink.java
+++ src/main/java/edu/ForceDrawnGraphs/models/Hyperlink.java
@@ -1,5 +1,7 @@
 package edu.ForceDrawnGraphs.models;
 
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
 public class Hyperlink {
   private int id;
   private int from_page_id;
@@ -8,13 +10,13 @@ public class Hyperlink {
   private int lineRef;
 
   /**
-   * Constructs a hyperlink with with from & to page IDs, a default count of 1 and the line reference. 
+   * Constructs a hyperlink with from and to page IDs, a default count of 1, and the line reference. 
    * 
-   * This implementation should only be used when creating a new hyperlink, before committing it to the database.
+   * This constructor should only be used when creating a new hyperlink before committing it to the database.
    *
    * @param from_page_id the ID of the source page
    * @param to_page_id   the ID of the target page
-   * @param lineRef     the line reference of the hyperlink
+   * @param lineRef      the line reference of the hyperlink
    */
   public Hyperlink(int from_page_id, int to_page_id, int lineRef) {
     this.from_page_id = from_page_id;
@@ -22,6 +24,25 @@ public class Hyperlink {
     this.count = 1;
     this.lineRef = lineRef;
   }
+
+  /**
+   * Constructs a hyperlink with from and to page IDs, a count, and the line reference. 
+   * 
+   * This constructor should only be used when creating a new hyperlink before committing it to the database.
+   *
+   * @param id            the ID of the hyperlink
+   * @param from_page_id  the ID of the source page
+   * @param to_page_id    the ID of the target page
+   * @param count         the count of the hyperlink
+   * @param lineRef       the line reference of the hyperlink
+   */
+  public Hyperlink(int id, int from_page_id, int to_page_id, int count, int lineRef) {
+    this.id = id;
+    this.from_page_id = from_page_id;
+    this.to_page_id = to_page_id;
+    this.count = count;
+    this.lineRef = lineRef;
+  };
 
   /**
    * Returns the ID of the hyperlink.
@@ -78,7 +99,7 @@ public class Hyperlink {
   }
 
   /**
-   * Sets the count of the hyperlink, count represents the number of times the relevant page is linked to.
+   * Sets the count of the hyperlink.
    *
    * @param count the count of the hyperlink
    */
@@ -109,5 +130,16 @@ public class Hyperlink {
    */
   public void incrementCount() {
     this.count++;
+  }
+
+  /**
+   * Maps the data from a SQL row set to a Hyperlink object.
+   *
+   * @param row the SQL row set containing the hyperlink data
+   * @return the Hyperlink object mapped from the SQL row set
+   */
+  public static Hyperlink mapSQLRowSetToHyperlink(SqlRowSet row) {
+    return new Hyperlink(row.getInt("id"), row.getInt("from_page_id"), row.getInt("to_page_id"),
+        row.getInt("count"), row.getInt("line_ref"));
   }
 }
---
+++ src/main/java/edu/ForceDrawnGraphs/models/Statement.java
@@ -0,0 +1,76 @@
+package edu.ForceDrawnGraphs.models;
+
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+/**
+ * Represents a statement in the dataset linking two (distinct) items. The srcItemID and tgtItemID are NON-Directional and only meant to provide a means of addressing either end of the statement edge.
+ */
+/**
+ * Represents a statement in the application.
+ * This class extends the BaseDatasetRecord class.
+ */
+public class Statement extends BaseDatasetRecord {
+  private int srcItemID;
+  private int tgtItemID;
+
+  /**
+   * Constructs a new Statement object with the specified ID, line reference, source item ID, and target item ID.
+   * 
+   * @param id The ID of the statement.
+   * @param lineRef The line reference of the statement.
+   * @param srcItemID The source item ID of the statement.
+   * @param tgtItemID The target item ID of the statement.
+   */
+  public Statement(int id, int lineRef, int srcItemID, int tgtItemID) {
+    super(id, lineRef);
+    this.srcItemID = srcItemID;
+    this.tgtItemID = tgtItemID;
+  }
+
+  /**
+   * Gets the source item ID of the statement.
+   * 
+   * @return The source item ID.
+   */
+  public int getSrcItemID() {
+    return srcItemID;
+  }
+
+  /**
+   * Sets the source item ID of the statement.
+   * 
+   * @param srcItemID The source item ID to set.
+   */
+  public void setSrcItemID(int srcItemID) {
+    this.srcItemID = srcItemID;
+  }
+
+  /**
+   * Gets the target item ID of the statement.
+   * 
+   * @return The target item ID.
+   */
+  public int getTgtItemID() {
+    return tgtItemID;
+  }
+
+  /**
+   * Sets the target item ID of the statement.
+   * 
+   * @param tgtItemID The target item ID to set.
+   */
+  public void setTgtItemID(int tgtItemID) {
+    this.tgtItemID = tgtItemID;
+  }
+
+  /**
+   * Maps a SQL row set to a Statement object.
+   * 
+   * @param results The SQL row set to map.
+   * @return A new Statement object mapped from the SQL row set.
+   */
+  public static Statement mapSqlRowSetToStatement(SqlRowSet results) {
+    return new Statement(results.getInt("id"), results.getInt("line_ref"),
+        results.getInt("source_item_id"), results.getInt("target_item_id"));
+  }
+}
