c24f0276bd6ca0ba29ff63a53ae19bb9efcbc19e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-06 20:14:01 -0600

    Sets up for grabbing finer timing details

--- src/main/java/edu/ForceDrawnGraphs/commands/Graphset.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/Graphset.java
@@ -2,8 +2,6 @@ package edu.ForceDrawnGraphs.commands;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
@@ -13,7 +11,7 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import edu.ForceDrawnGraphs.functions.GetPreparedStmt;
-
+import edu.ForceDrawnGraphs.interfaces.ProcessTimer;
 import edu.ForceDrawnGraphs.models.Item;
 import edu.ForceDrawnGraphs.models.Hyperlink;
 import edu.ForceDrawnGraphs.models.Page;
@@ -31,11 +29,17 @@ public class Graphset implements GetPreparedStmt {
 
   @ShellMethod
   public void demoset() {
-    Page page = getRandomPage();
+    // RANDOM PAGE
+    Page page = getRandomPage(); // Omit randomPageGetting from process timing.
+    // RANDOM PAGE
+    ProcessTimer timer = new ProcessTimer("demoset()");
+
     Item item = getItemByPage(page);
     Vertex vertex = Vertex.createNewVertexFromRecords(item, page);
+
     Set<Hyperlink> hyperlinks = getHyperlinksByPageID(page.getPageID());
     Set<Statement> statements = getStatementsByItemID(item.getItemID());
+
     Set<String> otherVertexIds = new HashSet<>();
   }
 
@@ -53,23 +57,28 @@ public class Graphset implements GetPreparedStmt {
   }
 
   private Item getItemByPage(Page page) {
+    ProcessTimer timer = new ProcessTimer("getItemByPage(" + page.getPageID() + ")");
     String sql = "SELECT * FROM items WHERE item_id = ?;";
     Item item = null;
 
     try {
       SqlRowSet results = jdbcTemplate.queryForRowSet(sql, page.getItemID());
       if (results.next()) {
+
         return Item.mapSqlRowSetToItem(results);
       } else {
         report("No item found for page " + page.getPageID());
       }
     } catch (Exception e) {
       report("Error on getItemByPage() in Graphset.java:", e);
+    } finally {
+      timer.end();
     }
     return item;
   }
 
   private Set<Hyperlink> getHyperlinksByPageID(String pageId) {
+    ProcessTimer timer = new ProcessTimer("getHyperlinksByPageID(" + pageId + ")");
     String sql = "SELECT * FROM hyperlinks WHERE from_page_id = ? OR to_page_id = ?;";
     Set<Hyperlink> hyperlinks = new HashSet<Hyperlink>();
     try {
@@ -79,11 +88,14 @@ public class Graphset implements GetPreparedStmt {
       }
     } catch (Exception e) {
       report("Error on getHyperlinksByPageID() in Graphset.java:", e);
+    } finally {
+      timer.end();
     }
     return hyperlinks;
   }
 
   private Set<Statement> getStatementsByItemID(String itemId) {
+    ProcessTimer timer = new ProcessTimer("getStatementsByItemID(" + itemId + ")");
     String sql = "SELECT * FROM statements WHERE source_item_id = ? OR target_item_id = ?;";
     Set<Statement> statements = new HashSet<Statement>();
     try {
@@ -93,6 +105,8 @@ public class Graphset implements GetPreparedStmt {
       }
     } catch (Exception e) {
       report("Error on getStatementsByItemID() in Graphset.java:", e);
+    } finally {
+      timer.end();
     }
     return statements;
   }
