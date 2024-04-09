c8153587f9d28f3f52315fcbf3d8406fea455f03
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-08 22:35:52 -0600

    first iteration creating n+1 vertex objects of random vertex and all realated
    next step should be edges

--- src/main/java/edu/ForceDrawnGraphs/commands/GraphsetCommands.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/GraphsetCommands.java
@@ -23,7 +23,11 @@ import edu.ForceDrawnGraphs.models.Vertex;
 @ShellComponent
 public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
   private JdbcTemplate jdbcTemplate;
+
   private Graphset graphset;
+
+  private Set<Item> itemCache = new HashSet<Item>();
+  private Set<Page> pageCache = new HashSet<Page>();
 
   public GraphsetCommands(DataSource datasource) {
     this.jdbcTemplate = new JdbcTemplate(datasource);
@@ -32,34 +36,51 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
 
   @ShellMethod
   public void demoset() {
+    // String urlEx = "https://en.wikipedia.org/wiki/Byandovan";
     ProcessTimer timer = new ProcessTimer("demoset()");
 
     // The vertex only requires knowledge of the page to be created (item whic may be null)
     Page page = getRandomPage();
     Item item = getItemByPage(page);
-    Vertex vertex = createAndAddVertex(page, item);
+    Vertex genesisVertex = createAndAddVertexToGraphset(page, item); // use return value as 'srcVertexID'
 
-    // Next is creating adn adding the edges
-    Set<Hyperlink> hyperlinks = getHyperlinksByPageID(page.getPageID()); // has a bunch src and tgt page ids in it you don't know yet
-    Set<Statement> statements = getStatementsByItemID(item.getItemID()); // has a bunch of src and tgt item ids in it you don't know yet
-
-    Set<String> targetPageIDS = new HashSet<String>();
-    Set<String> targetItemIDS = new HashSet<String>();
-
-    for (Hyperlink hyperlink : hyperlinks) {
-      if (hyperlink.getFromPageID().equals(page.getPageID())) {
-        targetPageIDS.add(hyperlink.getToPageID());
-      } else {
-        targetPageIDS.add(hyperlink.getFromPageID());
-      }
+    //! DIVIDER FOR VERTEX VS. EDGE CREATION IN THE PROCESS
+    Set<Hyperlink> relatedHyperlinks = new HashSet<Hyperlink>();
+    Set<Statement> relatedStatements = new HashSet<Statement>();
+    Set<String> relatedPageIDs = new HashSet<String>();
+    Set<String> relatedItemIDs = new HashSet<String>();
+    // Get and hyperlinks or statements which reference the page or item
+    if (page != null) {
+      relatedHyperlinks = getHyperlinksByPageID(page.getPageID());
+      relatedPageIDs = getUniqueTargetPageIDs(relatedHyperlinks, page.getPageID());
     }
 
-    for (Statement statement : statements) {
-      if (statement.getSrcItemID().equals(item.getItemID())) {
-        targetItemIDS.add(statement.getTgtItemID());
-      } else {
-        targetItemIDS.add(statement.getSrcItemID());
+    if (item != null) {
+      relatedStatements = getStatementsByItemID(item.getItemID());
+      relatedItemIDs = getUniqueTargetItemIDs(relatedStatements, item.getItemID());
+    }
+    // Check local graphset for existing references to target pages and items
+    for (Vertex vertex : graphset.getVertices()) {
+      if (relatedPageIDs.contains(vertex.getSrcPageId()) || relatedItemIDs.contains(vertex.getSrcItemId())) {
+        relatedPageIDs.remove(vertex.getSrcPageId());
+        relatedItemIDs.remove(vertex.getSrcItemId());
       }
+    }
+    //! DIVIDER FOR RELATED VERTEX CRTATION IN THE PROCESS
+    // Create and add vertices for each remaining target page and item
+    for (String relatedPageID : relatedPageIDs) {
+      Page relatedPage = getPageByID(relatedPageID);
+      Item relatedItem = getItemByPage(relatedPage);
+      createAndAddVertexToGraphset(relatedPage, relatedItem);
+      // relatedPageIDs.remove(relatedPageID); // causes concurrent modification exception
+      // ! This is a null return candidate for async execution
+    }
+    for (String relatedItemID : relatedItemIDs) {
+      Item relatedItem = getItemByID(relatedItemID);
+      Page relatedPage = getPageByItem(relatedItem);
+      createAndAddVertexToGraphset(relatedPage, relatedItem);
+      // relatedItemIDs.remove(relatedItemID); // causes concurrent modification exception
+      // ! This is a null return candidate for async execution
     }
 
     print("GOTTA STOP SOMEWHERE");
@@ -67,7 +88,43 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
     timer.end();
   }
 
+  private Page getPageByID(String pageID) {
+    ProcessTimer timer = new ProcessTimer("getPageByID(" + pageID + ")");
+
+    String sql = "SELECT * FROM pages WHERE page_id = ?;";
+    try {
+      SqlRowSet results = jdbcTemplate.queryForRowSet(sql, pageID);
+      if (results.next()) {
+        return Page.mapSQLRowSetToPage(results);
+      }
+    } catch (Exception e) {
+      report("Error on getPageByID() in Graphset.java:", e);
+    } finally {
+      timer.end();
+    }
+    return null;
+  }
+
+  private Item getItemByID(String itemID) {
+    ProcessTimer timer = new ProcessTimer("getItemByID(" + itemID + ")");
+
+    String sql = "SELECT * FROM items WHERE item_id = ?;";
+    try {
+      SqlRowSet results = jdbcTemplate.queryForRowSet(sql, itemID);
+      if (results.next()) {
+        return Item.mapSqlRowSetToItem(results);
+      }
+    } catch (Exception e) {
+      report("Error on getItemByID() in Graphset.java:", e);
+    } finally {
+      timer.end();
+    }
+    return null;
+  }
+
   private Page getRandomPage() {
+    ProcessTimer timer = new ProcessTimer("getRandomPage()");
+
     String sql = "SELECT * FROM pages ORDER BY RANDOM() LIMIT 1;";
     try {
       SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
@@ -76,12 +133,15 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
       }
     } catch (Exception e) {
       report("Error on getRandomPage() in Graphset.java:", e);
+    } finally {
+      timer.end();
     }
     return null;
   }
 
   private Item getItemByPage(Page page) {
     ProcessTimer timer = new ProcessTimer("getItemByPage(" + page.getPageID() + ")");
+
     String sql = "SELECT * FROM items WHERE item_id = ?;";
     Item item = null;
 
@@ -101,8 +161,30 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
     return item;
   }
 
+  private Page getPageByItem(Item item) {
+    ProcessTimer timer = new ProcessTimer("getPageByItem(" + item.getItemID() + ")");
+
+    String sql = "SELECT * FROM pages WHERE item_id = ?;";
+    Page page = null;
+
+    try {
+      SqlRowSet results = jdbcTemplate.queryForRowSet(sql, item.getItemID());
+      if (results.next()) {
+        return Page.mapSQLRowSetToPage(results);
+      } else {
+        report("No page found for item " + item.getItemID());
+      }
+    } catch (Exception e) {
+      report("Error on getPageByItem() in Graphset.java:", e);
+    } finally {
+      timer.end();
+    }
+    return page;
+  }
+
   private Set<Hyperlink> getHyperlinksByPageID(String pageId) {
     ProcessTimer timer = new ProcessTimer("getHyperlinksByPageID(" + pageId + ")");
+
     String sql = "SELECT * FROM hyperlinks WHERE from_page_id = ? OR to_page_id = ?;";
     Set<Hyperlink> hyperlinks = new HashSet<Hyperlink>();
     try {
@@ -120,6 +202,7 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
 
   private Set<Statement> getStatementsByItemID(String itemId) {
     ProcessTimer timer = new ProcessTimer("getStatementsByItemID(" + itemId + ")");
+
     String sql = "SELECT * FROM statements WHERE source_item_id = ? OR target_item_id = ?;";
     Set<Statement> statements = new HashSet<Statement>();
     try {
@@ -135,10 +218,51 @@ public class GraphsetCommands implements GetPreparedStmt, ReadSQLFileAsString {
     return statements;
   }
 
-  private Vertex createAndAddVertex(Page page, Item item) {
-    Vertex vertex = Vertex.createNewVertexFromRecords(item, page);
+  private Vertex createAndAddVertexToGraphset(Page page, Item item) {
+    ProcessTimer timer = new ProcessTimer("createAndAddVertexToGraphset();");
+    Vertex vertex = null;
+    if (page == null || item == null) {
+      report("No page or item found for vertex creation.");
+    }
+    if (item != null && page != null) {
+      vertex = new Vertex(item, page);
+    } else if (item != null) {
+      vertex = new Vertex(item);
+    } else if (page != null) {
+      vertex = new Vertex(page);
+    }
+
     graphset.addVertex(vertex);
+    timer.end();
     return vertex;
+  }
+
+  private Set<String> getUniqueTargetPageIDs(Set<Hyperlink> hyperlinks, String pageID) {
+    ProcessTimer timer = new ProcessTimer("getUniqueTargetPageIDs(" + pageID + ")");
+    Set<String> targetPageIDS = new HashSet<String>();
+    for (Hyperlink hyperlink : hyperlinks) {
+      if (hyperlink.getFromPageID().equals(pageID)) {
+        targetPageIDS.add(hyperlink.getToPageID());
+      } else {
+        targetPageIDS.add(hyperlink.getFromPageID());
+      }
+    }
+    timer.end();
+    return targetPageIDS;
+  }
+
+  private Set<String> getUniqueTargetItemIDs(Set<Statement> statements, String itemID) {
+    ProcessTimer timer = new ProcessTimer("getUniqueTargetItemIDs(" + itemID + ")");
+    Set<String> targetItemIDS = new HashSet<String>();
+    for (Statement statement : statements) {
+      if (statement.getSrcItemID().equals(itemID)) {
+        targetItemIDS.add(statement.getTgtItemID());
+      } else {
+        targetItemIDS.add(statement.getSrcItemID());
+      }
+    }
+    timer.end();
+    return targetItemIDS;
   }
 
 }
