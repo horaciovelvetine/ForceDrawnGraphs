027e6bc6489e4bb7df3d85a4a64c42501d7f06f6
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-11 22:04:33 -0600

    adds indexing and #_of_references col

--- src/main/resources/sql/CreateProperties.sql
+++ src/main/resources/sql/CreateProperties.sql
@@ -4,5 +4,8 @@ CREATE TABLE properties (
     id SERIAL PRIMARY KEY,
     property_id VARCHAR(255),
     en_label VARCHAR(255),
-    en_description TEXT
+    en_description TEXT,
+    number_of_references INT DEFAULT 0
 );
+
+CREATE INDEX idx_property_id ON properties (property_id);
