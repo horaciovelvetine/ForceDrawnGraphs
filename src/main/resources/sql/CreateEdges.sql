45351cb7236b48a5cf3fb295976f5feda74ef22d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-11 22:18:12 -0600

    reflects changes on model

--- src/main/resources/sql/CreateEdges.sql
+++ src/main/resources/sql/CreateEdges.sql
@@ -4,9 +4,10 @@ CREATE TABLE edges (
   id SERIAL PRIMARY KEY,
   src_vertex_id INT,
   tgt_vertex_id INT,
-  weight VARCHAR(255),
-  edge_type VARCHAR(255),
-  edge_property_id VARCHAR(255)
+  weight INT,
+  edge_property_id VARCHAR(255),
+  src_statment_id INT,
+  src_hyperlink_id INT
 );
 
 CREATE INDEX idx_src_vertex_id ON edges (src_vertex_id);
