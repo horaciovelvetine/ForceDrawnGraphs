7346b9b5248596266486744f9fb05cdce15ca86c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-12 18:57:20 -0600

    add both label and title for vertices that source from both

--- src/main/resources/sql/CreateVertices.sql
+++ src/main/resources/sql/CreateVertices.sql
@@ -6,6 +6,7 @@ CREATE TABLE vertices (
     y FLOAT,
     z FLOAT,
     label VARCHAR(255),
+    title VARCHAR(255),
     en_description TEXT,
     views VARCHAR(255),
     src_item_id VARCHAR(255),
