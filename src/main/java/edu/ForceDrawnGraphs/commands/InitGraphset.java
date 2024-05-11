fac6aeb96a214e8135dfbcf197ffb2862797ff46
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-05-10 21:13:54 -0600

    remove my own jackson imports instead inhering that through wikidata, this was causing result parsing issues on get requests to the API - interesting
    gets a default document by title from the api

--- src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
@@ -1,5 +1,24 @@
 package edu.ForceDrawnGraphs.commands;
 
-public class InitGraphset {
-  
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
+import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
+
+import edu.ForceDrawnGraphs.functions.FindWMEntDocumentFromTitleQuery;
+import edu.ForceDrawnGraphs.interfaces.ProcessTimer;
+
+@ShellComponent
+public class InitGraphset implements FindWMEntDocumentFromTitleQuery {
+  private static WikibaseDataFetcher wbdf = WikibaseDataFetcher.getWikidataDataFetcher();
+
+  @ShellMethod("Create a graphset (JSON file) given an origin target (or a default of Kevin Bacon).")
+  public void initGraphset(@ShellOption(defaultValue = "Kevin Bacon") String target) {
+    ProcessTimer timer = new ProcessTimer("initGraphset(" + target + ") in InitGraphset.java");
+    EntityDocument entityDocument = findWMEntDocumentFromQuery(target, wbdf);
+    print("Stops here");
+  }
 }
