d7213b8c261087ee5122b71f8337c61bb85a3f54
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-05-16 22:03:24 -0600

    init design aligning with JUNG doc reading

--- src/main/java/edu/ForceDrawnGraphs/models/Edge.java
+++ src/main/java/edu/ForceDrawnGraphs/models/Edge.java
@@ -1,5 +1,32 @@
 package edu.ForceDrawnGraphs.models;
 
+import java.util.Objects;
+
 public class Edge {
-  
+  private String srcVertexQID;
+  private String tgtVertexQID;
+  private double weight;
+
+  public Edge(String srcVertexQID, String tgtVertexQID, double weight) {
+    this.srcVertexQID = srcVertexQID;
+    this.tgtVertexQID = tgtVertexQID;
+    this.weight = weight;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Edge edge = (Edge) o;
+    return Double.compare(edge.weight, weight) == 0 &&
+        Objects.equals(srcVertexQID, edge.srcVertexQID) &&
+        Objects.equals(tgtVertexQID, edge.tgtVertexQID);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(srcVertexQID, tgtVertexQID, weight);
+  }
 }
