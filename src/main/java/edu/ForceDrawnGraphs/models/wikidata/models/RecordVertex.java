2f060d7786e4e6678057e3f528d4bcbb357883cd
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-01 01:45:04 -0600

    delete for more name changes

--- src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/RecordVertex.java
+++
@@ -1,9 +0,0 @@
-package edu.ForceDrawnGraphs.models.wikidata.models;
-
-import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-
-public record RecordVertex(String QID, String label, String description) {
-  public RecordVertex(ItemDocument doc) {
-    this(doc.getEntityId().getId(), doc.findLabel("en"), doc.findDescription("en"));
-  }
-}
