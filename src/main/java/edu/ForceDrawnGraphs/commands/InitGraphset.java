882f09f518ef46702dbb6e78a3c2b4ac9b40fd84
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-01 01:48:51 -0600

    changes reflect ongoin thinning and attempt to make easier to follow

--- src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
@@ -8,25 +8,23 @@ import edu.ForceDrawnGraphs.interfaces.Reportable;
 import edu.ForceDrawnGraphs.models.Graphset;
 import edu.ForceDrawnGraphs.models.wikidata.services.APIBroker;
 import edu.ForceDrawnGraphs.models.wikidata.services.EntDocProc;
-import edu.ForceDrawnGraphs.util.ProcessTimer;
 
 @ShellComponent
 public class InitGraphset implements Reportable {
   private static Graphset graphset = new Graphset();
   private static EntDocProc docProc = new EntDocProc(graphset);
   private static APIBroker brokerService = new APIBroker(docProc);
-  private int fetchDepth = 2;
+  private int fetchDepth = 100;
 
   @ShellMethod("Create a graphset (JSON file) given an origin target (or a default of Kevin Bacon).")
   public void ig(@ShellOption(defaultValue = "Kevin Bacon") String target) {
-    ProcessTimer timer = new ProcessTimer("initGraphset(" + target + ") in InitGraphset.java");
+    graphset.setOriginQuery(target); // set the origin query for the graphset
     brokerService.fuzzyFetchOriginEntityDocument(target); // fetch origin entity document, and init graphset data management
 
     while (graphset.fetchQueue().hasItems() && fetchDepth-- > 0) {
-      // should iterate over the fetchQueue, fetching until nothing remains
-      brokerService.fetchItemsInQueue(graphset);
+      // TODO iterate over the fetchQueue, fetching until nothing remains
+      // brokerService.fetchItemsInQueue(graphset);
     }
-    timer.end();
-    print("Graphset initialized.");
+    print("Stop graphset end init.");
   }
 }
