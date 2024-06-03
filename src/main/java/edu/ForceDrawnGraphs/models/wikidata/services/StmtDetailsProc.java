ec01e42e879362e70c7ec9258717aa08c124f01d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-02 22:58:41 -0600

    continue fetching stmt details, adds exlusions and sets at top of class (more clear and easier to find)

--- src/main/java/edu/ForceDrawnGraphs/models/wikidata/services/StmtDetailsProc.java
+++ src/main/java/edu/ForceDrawnGraphs/models/wikidata/services/StmtDetailsProc.java
@@ -2,6 +2,7 @@ package edu.ForceDrawnGraphs.models.wikidata.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.wikidata.wdtk.datamodel.interfaces.Snak;
 import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
@@ -9,10 +10,11 @@ import org.wikidata.wdtk.datamodel.interfaces.Statement;
 
 import edu.ForceDrawnGraphs.interfaces.Reportable;
 import edu.ForceDrawnGraphs.models.Edge;
+import edu.ForceDrawnGraphs.models.Graphset;
 import edu.ForceDrawnGraphs.models.wikidata.models.UnknownSnakVisitor;
 import edu.ForceDrawnGraphs.models.wikidata.models.WikiDataEdge;
 import edu.ForceDrawnGraphs.models.wikidata.models.WikiRecSnak;
-import edu.ForceDrawnGraphs.models.wikidata.models.WikiDataEdge.EDGE_SRC;
+import edu.ForceDrawnGraphs.models.wikidata.models.WikiDataEdge.SNAK_SRC;;
 
 /**
  * Processes statement details to extract relevant information for graph visualization.
@@ -21,7 +23,30 @@ class StmtDetailsProcessor implements Reportable {
   private final UnknownSnakVisitor snakVisitor = new UnknownSnakVisitor();
   private final WikiRecSnak mainSnak;
   private final List<WikiRecSnak[]> qualifiers;
-  private final List<Edge> edges = new ArrayList<>(); // all edges derived from the statement 
+  private final List<Edge> edges = new ArrayList<>(); // all edges derived from the statement details
+  private static final Set<String> EXCLUDED_DATA_TYPES = Set.of(
+      "external-id",
+      "monolingualtext",
+      "commonsMedia",
+      "url",
+      "globe-coordinate",
+      "geo-shape");
+  private static final Set<String> KNOWN_INCLUDED_DATA_TYPES = Set.of(
+      "wikibase-item",
+      "wikibase-property",
+      "wikibase-lexeme", //TODO: These edges are "experimental" and likely to be removed
+      "string",
+      "quantity",
+      "time");
+  private static final Set<String> EXCLUDED_PROPERTIES = Set.of(
+      "P1343",
+      "P143",
+      "P935",
+      "P8687",
+      "P3744",
+      "P18",
+      "P373",
+      "P856");
 
   /**
    * Constructs a processor for a given Wikidata statement.
@@ -48,8 +73,7 @@ class StmtDetailsProcessor implements Reportable {
    * @return True if the statement is considered irrelevant or external, false otherwise.
    */
   public boolean definesIrrelevantOrExternalInfo() {
-    return mainSnak == null || isExcludedDataType(mainSnak) || isExcludedProperty(mainSnak)
-        || hasExcludedQualifierProperties();
+    return mainSnak == null || isExcludedDataType(mainSnak) || isExcludedProperty(mainSnak);
   }
 
   /**
@@ -58,16 +82,27 @@ class StmtDetailsProcessor implements Reportable {
    * @param srcVertexQID The QID of the source vertex for the edges.
    */
   public void createEdgesFromStmtDetails(String srcVertexQID) {
-    WikiDataEdge mainEdgeContext = createEdgeFromSnak(mainSnak, srcVertexQID, null, EDGE_SRC.MAIN_SNAK);
+    WikiDataEdge mainEdgeContext = createEdgeFromSnak(mainSnak, srcVertexQID, null, SNAK_SRC.MAIN_SNAK);
 
     if (qualifiers != null) {
       for (WikiRecSnak[] group : qualifiers) {
         for (WikiRecSnak snak : group) {
-          createEdgeFromSnak(snak, srcVertexQID, mainEdgeContext, EDGE_SRC.QUALIFIER);
+          // Skip excluded qualifier snaks on same critieria as main snak...
+          if (isExcludedDataType(snak) || isExcludedProperty(snak)) {
+            continue;
+          }
+          createEdgeFromSnak(snak, srcVertexQID, mainEdgeContext, SNAK_SRC.QUALIFIER);
         }
       }
     }
   }
+  //------------------------------------------------------------------------------------------------------------
+  //
+  //
+  //! PRIVATE METHODS // PRIVATE METHODS // PRIVATE METHODS // PRIVATE METHODS // PRIVATE METHODS // PRIVATE METHODS
+  //
+  //
+  //------------------------------------------------------------------------------------------------------------
 
   /**
    * Creates an edge from a given Snak and adds it to the list of edges.
@@ -79,11 +114,12 @@ class StmtDetailsProcessor implements Reportable {
    * @return The created WikiDataEdge.
    */
   private WikiDataEdge createEdgeFromSnak(WikiRecSnak snak, String srcVertexQID, WikiDataEdge contextEdge,
-      EDGE_SRC edgeSource) {
+      SNAK_SRC edgeSource) {
     WikiDataEdge edge = null;
 
     String contextQID = contextEdge != null ? contextEdge.propertyQID() : null;
     String contextValue = contextEdge != null ? contextEdge.tgtVertexQID() : null;
+
     if (contextEdge != null && contextValue == null) {
       contextValue = contextEdge.value();
     }
@@ -91,22 +127,30 @@ class StmtDetailsProcessor implements Reportable {
     switch (snak.value().type()) {
       case ENTITY:
         edge = new WikiDataEdge(srcVertexQID, snak.value().value(), snak.property().value(), null,
-            contextQID, contextValue, edgeSource);
+            contextQID, contextValue, edgeSource, snak.datatype());
         break;
+      //TODO STRING VALUE PROCESSING
       case STRING:
       case QUANT:
       case TIME:
         edge = new WikiDataEdge(srcVertexQID, null, snak.property().value(), snak.value().value(),
-            contextQID, contextValue, edgeSource);
+            contextQID, contextValue, edgeSource, snak.datatype());
         break;
     }
 
     if (edge != null) {
       edges.add(edge);
     }
-
     return edge;
   }
+
+  //------------------------------------------------------------------------------------------------------------
+  //
+  //
+  //* ENT VALIDATION METHODS - ENT VALIDATION METHODS - ENT VALIDATION METHODS - ENT VALIDATION METHODS  
+  //
+  //
+  //------------------------------------------------------------------------------------------------------------
 
   /**
    * Checks if the Snak's datatype is among the excluded types.
@@ -115,7 +159,15 @@ class StmtDetailsProcessor implements Reportable {
    * @return True if the datatype is excluded, false otherwise.
    */
   private boolean isExcludedDataType(WikiRecSnak snak) {
-    return "external-id".equals(snak.datatype()) || "monolingualtext".equals(snak.datatype());
+    if (snak == null) {
+      return true;
+    }
+    //TODO REMOVE AS TESTING CONSISTENTLY SHOWS NO MISSED DATA TYPES
+    if (!KNOWN_INCLUDED_DATA_TYPES.contains(snak.datatype()) && !EXCLUDED_DATA_TYPES.contains(snak.datatype())) {
+      report("Unknown datatype found:", snak.datatype());
+      return true;
+    }
+    return EXCLUDED_DATA_TYPES.contains(snak.datatype());
   }
 
   /**
@@ -125,39 +177,17 @@ class StmtDetailsProcessor implements Reportable {
    * @return True if the property is excluded, false otherwise.
    */
   private boolean isExcludedProperty(WikiRecSnak snak) {
-    String[] excludedProperties = { "P1343", "P143", "P935", "P8687", "P3744", "P18", "P373", "P856" };
-    return isPropertyInList(snak.property().value(), excludedProperties);
-  }
-
-  /**
-   * Checks if any qualifier properties are among the excluded properties.
-   *
-   * @return True if any qualifier property is excluded, false otherwise.
-   */
-  private boolean hasExcludedQualifierProperties() {
-    if (qualifiers == null) {
-      return false;
-    }
-
-    for (WikiRecSnak[] group : qualifiers) {
-      for (WikiRecSnak snak : group) {
-        if (isExcludedProperty(snak)) {
-          return true;
-        }
-      }
-    }
-    return false;
+    return isPropertyInList(snak.property().value());
   }
 
   /**
    * Helper method to check if a property ID is in a given list.
    *
    * @param propertyId The property ID to check.
-   * @param propertyList The list of property IDs to compare against.
    * @return True if the property ID is found in the list, false otherwise.
    */
-  private boolean isPropertyInList(String propertyId, String[] propertyList) {
-    for (String id : propertyList) {
+  private boolean isPropertyInList(String propertyId) {
+    for (String id : EXCLUDED_PROPERTIES) {
       if (propertyId.equals(id)) {
         return true;
       }
