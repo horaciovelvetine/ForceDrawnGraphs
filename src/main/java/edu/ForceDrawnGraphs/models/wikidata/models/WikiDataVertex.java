b859f60cbeaef2c1939a685d94035ca32fc46c6f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-02 22:54:36 -0600

    reflects similar pattern of edge, extending superclass

---
+++ src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/WikiDataVertex.java
@@ -0,0 +1,38 @@
+package edu.ForceDrawnGraphs.models.wikidata.models;
+
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+
+import edu.ForceDrawnGraphs.models.Vertex;
+
+public class WikiDataVertex extends Vertex {
+  private String description;
+
+  public WikiDataVertex(ItemDocument doc) {
+    super(doc.getEntityId().getId(), doc.findLabel("en"));
+    this.description = doc.findDescription("en");
+  }
+
+  public String QID() {
+    return ID();
+  }
+
+  public void setQID(String QID) {
+    this.setID(QID);
+  }
+
+  public String label() {
+    return label();
+  }
+
+  public void setLabel(String label) {
+    this.setLabel(label);
+  }
+
+  public String description() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
