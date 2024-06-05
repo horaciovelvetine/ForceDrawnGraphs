b8ff84ab017388597266c90a10a4b1e5a587f868
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-05 00:22:54 -0600

    full no go on string and quant values implemented
    added note to consider if this was to extreme and if quant and values might be relevant for qualifier snaks

--- src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/UnknownValueVisitor.java
+++ src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/UnknownValueVisitor.java
@@ -2,7 +2,6 @@ package edu.ForceDrawnGraphs.models.wikidata.models;
 
 import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.GlobeCoordinatesValue;
-import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.MonolingualTextValue;
 import org.wikidata.wdtk.datamodel.interfaces.QuantityValue;
 import org.wikidata.wdtk.datamodel.interfaces.StringValue;
@@ -19,9 +18,6 @@ import edu.ForceDrawnGraphs.interfaces.Reportable;
  */
 public class UnknownValueVisitor implements ValueVisitor<WikiRecValue>, Reportable {
 
-  /**
-   * Default constructor.
-   */
   public UnknownValueVisitor() {
     // Default constructor
   }
@@ -38,33 +34,7 @@ public class UnknownValueVisitor implements ValueVisitor<WikiRecValue>, Reportab
   }
 
   /**
-   * Visits a QuantityValue and returns a WikiRecValue.
-   *
-   * @param value the QuantityValue to visit.
-   * @return a WikiRecValue representing the QuantityValue.
-   */
-  @Override
-  public WikiRecValue visit(QuantityValue value) {
-    ItemIdValue unitItem = value.getUnitItemId();
-    String url = (unitItem == null) ? "n/a (see property definition)" : unitItem.getIri();
-    String text = value.toString();
-
-    return new WikiRecValue(WikiRecValue.TXT_VAL_TYPE.QUANT, text, url);
-  }
-
-  /**
-   * Visits a StringValue and returns a WikiRecValue.
-   *
-   * @param value the StringValue to visit.
-   * @return a WikiRecValue representing the StringValue.
-   */
-  @Override
-  public WikiRecValue visit(StringValue value) {
-    return new WikiRecValue(value);
-  }
-
-  /**
-   * Visits a TimeValue and returns a WikiRecValue.
+   * Visits a TimeValue and returns a WikiRecValue, formatting the string to just YYYY-MM-DD.
    *
    * @param value the TimeValue to visit.
    * @return a WikiRecValue representing the TimeValue.
@@ -78,6 +48,16 @@ public class UnknownValueVisitor implements ValueVisitor<WikiRecValue>, Reportab
   /**
    * These value types are not relevant to the application, and will simply be ignored.
    */
+  @Override
+  public WikiRecValue visit(QuantityValue value) {
+    return null;
+  }
+
+  @Override
+  public WikiRecValue visit(StringValue value) {
+    return null;
+  }
+
   @Override
   public WikiRecValue visit(UnsupportedValue value) {
     return null;
