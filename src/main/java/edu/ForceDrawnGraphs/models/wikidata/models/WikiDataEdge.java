266289a431caa262fd250fe24df59192bcfe3224
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-05 00:26:25 -0600

    refactored more detailed and better suited to handle grouping

--- src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/WikiDataEdge.java
+++ src/main/java/edu/ForceDrawnGraphs/models/wikidata/models/WikiDataEdge.java
@@ -1,44 +1,36 @@
 package edu.ForceDrawnGraphs.models.wikidata.models;
 
 import edu.ForceDrawnGraphs.models.Edge;
+import edu.ForceDrawnGraphs.models.wikidata.models.WikiRecValue.TXT_VAL_TYPE;
 
 public class WikiDataEdge extends Edge {
-  private String propertyQID; // QID of the Property
+  private String propertyQID;
   private String value; // either QID/or String: (date, quantity, actual string, url... etc)
-  private SNAK_SRC snakType; // the WikiEnt from which the edge was constructed
-  private String datatype; // the string value for .datatype() from the original snak
+  private String datatype; // the string value for .datatype() from the snak
+  private SNAK_SRC snakType; // MAIN_SNAK or QUALIFIER
+  // below values are only used when the snak source is a Qualifier
+  private ContextSnakRec contextSnakRec;
 
-  // below values are only used when snakType is QUALIFIER
-  private String contextPropertyQID; // QID of the a property to give context to this edge
-  private String contextValue;
+  public WikiDataEdge(WikiRecSnak snak, String srcVertexQID) {
+    super(srcVertexQID, findSnakTargetQID(snak));
+    this.snakType = SNAK_SRC.MAIN_SNAK;
+    this.propertyQID = snak.property().value();
+    this.value = findSnakTargetValue(snak);
+    this.datatype = snak.datatype();
+    this.contextSnakRec = null;
+  }
 
-  /**
-   * Constructor for a QUALIFIER sourced edge, with a property and value
-   * 
-   * @param srcVertexQID
-   * @param tgtVertexQID
-   * @param propertyQID
-   * @param value
-   * @param contextPropertyQID
-   * @param contextValue
-   * @param snakType
-   * @param datatype
-   * 
-   */
-  public WikiDataEdge(String srcVertexQID, String tgtVertexQID, String propertyQID, String value,
-      String contextPropertyQID, String contextValue, SNAK_SRC snakType, String datatype) {
-    super(srcVertexQID, tgtVertexQID);
-    this.propertyQID = propertyQID;
-    this.value = value;
-    this.contextPropertyQID = contextPropertyQID;
-    this.contextValue = contextValue;
-    this.snakType = snakType;
-    this.datatype = datatype;
+  public WikiDataEdge(WikiRecSnak snak, WikiDataEdge mainEdgeContext, int groupID) {
+    super(mainEdgeContext.tgtVertexQID(), findSnakTargetQID(snak));
+    this.snakType = SNAK_SRC.QUALIFIER;
+    this.propertyQID = snak.property().value();
+    this.value = findSnakTargetValue(snak);
+    this.datatype = snak.datatype();
+    this.contextSnakRec = new ContextSnakRec(mainEdgeContext, groupID);
   }
 
   public enum SNAK_SRC {
-    MAIN_SNAK,
-    QUALIFIER
+    MAIN_SNAK, QUALIFIER
   }
 
   public String propertyQID() {
@@ -53,15 +45,49 @@ public class WikiDataEdge extends Edge {
     return snakType;
   }
 
-  public String contextPropertyQID() {
-    return contextPropertyQID;
-  }
-
-  public String contextValue() {
-    return contextValue;
-  }
-
   public String datatype() {
     return datatype;
+  }
+
+  public ContextSnakRec contextSnakRec() {
+    return contextSnakRec;
+  }
+
+  @Override
+  public String toString() {
+    return "WikiDataEdge [propertyQID=" + propertyQID + ", value=" + value + ", snakType="
+        + snakType + ", datatype=" + datatype + "]";
+  }
+
+  /**
+   * Determines if this is a TIME value, which will not yet have a QID (target) for the Vertex. 
+   * If this is the case, the QID returned will be null.
+   */
+  private static String findSnakTargetQID(WikiRecSnak snak) {
+    if (snak.value().type() != TXT_VAL_TYPE.ENTITY)
+      return null;
+    return snak.value().value();
+  }
+
+  /**
+   * Determines if this is a TIME value, will return that value to set on qualifier Edges. 
+   */
+  private static String findSnakTargetValue(WikiRecSnak snak) {
+    if (snak.value().type() == TXT_VAL_TYPE.TIME)
+      return snak.value().value();
+    return null;
+  }
+
+  /**
+   * A private record to hold the additional details of a Qualifier Snak.
+   */
+  private record ContextSnakRec(String srcVertexQID, String tgtVertexQID, String propertyQID,
+      String value, String type, int groupID) {
+
+    public ContextSnakRec(WikiDataEdge mainEdgeContext, int groupID) {
+      this(mainEdgeContext.srcVertexQID(), mainEdgeContext.tgtVertexQID(),
+          mainEdgeContext.propertyQID(), mainEdgeContext.value(), mainEdgeContext.datatype(),
+          groupID);
+    }
   }
 }
