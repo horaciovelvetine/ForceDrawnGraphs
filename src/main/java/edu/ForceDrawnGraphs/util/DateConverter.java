69a706127af6e5d4051b6fab03f9d03e20293703
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-13 00:14:02 -0600

    adds some docs

--- src/main/java/edu/ForceDrawnGraphs/util/DateConverter.java
+++ src/main/java/edu/ForceDrawnGraphs/util/DateConverter.java
@@ -4,6 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.text.ParseException;
 
+/**
+ * Provides a way to convert the stored data format (string) from Wikidata to a more human-readable format, 
+ * this also allows for date entities to be searched for and found consistently (from the same API).
+ */
 public class DateConverter {
   public static String convertDate(String inputDate) {
     SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
