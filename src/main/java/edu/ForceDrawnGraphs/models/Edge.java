58d899c8ca715415a6bd55e491e2f9a186d18149
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-13 00:12:38 -0600

    add setter for tgt QID to let ent know it has related prop

--- src/main/java/edu/ForceDrawnGraphs/models/Edge.java
+++ src/main/java/edu/ForceDrawnGraphs/models/Edge.java
@@ -21,6 +21,10 @@ public class Edge {
     return tgtVertexID;
   }
 
+  public void setTgtVertexID(String tgtID) {
+    this.tgtVertexID = tgtID;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o)
