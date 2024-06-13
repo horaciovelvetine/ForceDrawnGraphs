365c66c12785092f0c36c70fd84351bb28986de1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-13 00:14:37 -0600

    adds vertex constructor from search result passed in for new Date vertices

--- src/main/java/edu/ForceDrawnGraphs/wikidata/models/WikiDataVertex.java
+++ src/main/java/edu/ForceDrawnGraphs/wikidata/models/WikiDataVertex.java
@@ -1,38 +1,45 @@
 package edu.ForceDrawnGraphs.wikidata.models;
 
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-
+import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
 import edu.ForceDrawnGraphs.models.Vertex;
 
 public class WikiDataVertex extends Vertex {
   private String description;
+  private String matchingPropertyQID;
 
   public WikiDataVertex(ItemDocument doc) {
     super(doc.getEntityId().getId(), doc.findLabel("en"));
     this.description = doc.findDescription("en");
   }
 
-  public String QID() {
-    return ID();
+  public WikiDataVertex(WbSearchEntitiesResult result) {
+    super(result.getEntityId(), result.getLabel());
+    this.description = result.getDescription();
   }
 
-  public void setQID(String QID) {
-    this.setID(QID);
+  public String QID() {
+    return ID();
   }
 
   public String label() {
     return label();
   }
 
-  public void setLabel(String label) {
-    this.setLabel(label);
-  }
-
   public String description() {
     return description;
   }
 
-  public void setDescription(String description) {
-    this.description = description;
+  public void setMatchingPropertyQID(String matchingPropertyQID) {
+    this.matchingPropertyQID = matchingPropertyQID;
   }
+
+  public String matchingPropertyQID() {
+    return matchingPropertyQID;
+  }
+
+  public boolean hasMatchingPropertyQID() {
+    return matchingPropertyQID != null;
+  }
+
 }
