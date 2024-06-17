273c5aaecc343b4879b18060b4a9cea07f5d6004
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-17 17:44:55 -0600

    pull threshold up to the choice of the method call to be able to set as needed

--- src/main/java/edu/ForceDrawnGraphs/util/FuzzyStringMatch.java
+++ src/main/java/edu/ForceDrawnGraphs/util/FuzzyStringMatch.java
@@ -9,52 +9,36 @@ import java.util.Comparator;
 import edu.ForceDrawnGraphs.models.Vertex;
 
 public class FuzzyStringMatch {
-  public static Integer MATCH_THRESHOLD = 11;
-
   /**
    * Fuzzy matches a target string to a list of vertices.
    * 
    * @param target   The target string to match.
    * @param vertices The list of vertices to match against.
+   * @param threshold An arbitrary threshold value to establish match strength.
    * @return A list of vertices that match the target string.
    */
-  public static List<Vertex> fuzzyMatch(String target, List<Vertex> vertices) {
+  public static List<Vertex> fuzzyMatch(String target, List<Vertex> vertices, Integer threshold) {
     List<VertexDistance> distances = new ArrayList<>();
     for (Vertex vertex : vertices) {
       int distance = LevenshteinStringMatch.computeLevenshteinDistance(target, vertex.label());
       distances.add(new VertexDistance(vertex, distance));
     }
 
-    Collections.sort(distances, Comparator.comparingInt(VertexDistance::getDistance));
+    Collections.sort(distances, Comparator.comparingInt(VertexDistance::distance));
 
     List<Vertex> result = new ArrayList<>();
     for (VertexDistance vertexDistance : distances) {
-      if (vertexDistance.getDistance() < MATCH_THRESHOLD) {
-        result.add(vertexDistance.getVertex());
+      if (vertexDistance.distance() < threshold) {
+        result.add(vertexDistance.vertex());
       }
     }
     return result;
   }
 
-  private static class VertexDistance {
-    private Vertex vertex;
-    private int distance;
-
-    public VertexDistance(Vertex vertex, int distance) {
-      this.vertex = vertex;
-      this.distance = distance;
-    }
-
-    public Vertex getVertex() {
-      return vertex;
-    }
-
-    public int getDistance() {
-      return distance;
-    }
+  public static List<Vertex> fuzzyMatch(String label, Set<Vertex> vertices, Integer threshold) {
+    return fuzzyMatch(label, new ArrayList<>(vertices), threshold);
   }
 
-  public static List<Vertex> fuzzyMatch(String label, Set<Vertex> vertices) {
-    return fuzzyMatch(label, new ArrayList<>(vertices));
+  private static record VertexDistance(Vertex vertex, int distance) {
   }
 }
