461e6e9171228f9bc8dc75698139392ee2ae86bd
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-21 23:27:09 -0600

    util to serialize and deserialize datasets for consistent testing locally

---
+++ src/main/java/edu/ForceDrawnGraphs/util/FirstResponder.java
@@ -0,0 +1,30 @@
+package edu.ForceDrawnGraphs.util;
+
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ForceDrawnGraphs.models.Graphset;
+
+public class FirstResponder {
+  // as an homage to those who respond first, I bravely trudge on.
+  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final String STORE_DIR = "src/main/resources/data/"; // to keep a testable consistent JSON copy of the Data
+
+  public static void serializeResponset(Graphset graphset) {
+    try {
+      mapper.writeValue(new File(STORE_DIR + "graphset.json"), graphset);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static Graphset desrializeResponset() {
+    try {
+      return mapper.readValue(new File(STORE_DIR + "graphset.json"), Graphset.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+}
