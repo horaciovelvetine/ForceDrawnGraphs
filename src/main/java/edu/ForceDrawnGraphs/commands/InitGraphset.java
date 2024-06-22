712d7e530e75acfd66113a0ae323c54e674b7112
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-21 22:41:59 -0600

    remove first graph layouts to some helper methods inside of the decorator - working successfully to generate graphs

--- src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/InitGraphset.java
@@ -1,25 +1,15 @@
 package edu.ForceDrawnGraphs.commands;
 
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
-import javax.swing.JFrame;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 import edu.ForceDrawnGraphs.interfaces.Reportable;
 import edu.ForceDrawnGraphs.jung.GraphsetDecorator;
-import edu.ForceDrawnGraphs.models.Edge;
 import edu.ForceDrawnGraphs.models.Graphset;
-import edu.ForceDrawnGraphs.models.Vertex;
 import edu.ForceDrawnGraphs.util.ProcessTimer;
 import edu.ForceDrawnGraphs.wikidata.services.APIBroker;
 import edu.ForceDrawnGraphs.wikidata.services.EntDocProc;
-
-import edu.uci.ics.jung.algorithms.layout.FRLayout;
-
-import edu.uci.ics.jung.visualization.VisualizationViewer;
-import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 
 @ShellComponent
 public class InitGraphset implements Reportable {
@@ -30,8 +20,7 @@ public class InitGraphset implements Reportable {
   //JUNG
   private static GraphsetDecorator graphsetDec = new GraphsetDecorator(graphset);
   // Configuration
-  private static Integer targetDepth = 2;
-  private static Dimension graphSize = new Dimension(3200, 1600); // from cx by default? 
+  private static Integer targetDepth = 3;
 
   @ShellMethod("Create a graphset (JSON file) given an origin target (or a default of Kevin Bacon).")
   public void ig(@ShellOption(defaultValue = "Kevin Bacon") String target) {
@@ -52,56 +41,13 @@ public class InitGraphset implements Reportable {
         graphset.iterateNDepth();
       }
 
-      report(graphset.toString() + "\n[Vertices: " + graphsetDec.getVertexCount() + " Edges: "
-          + graphsetDec.getEdgeCount() + "]\n");
       timer.lap();
     }
-
-    //TODO - end of the fetch (sh?)could close the Vertex data (by fetching remaining ItemDocs)
-    //TODO - have the edges look up labels before adding to the graphsetDec
-
     timer.end();
-
-    ProcessTimer timer2 = new ProcessTimer("init FRLatout()::");
-    FRLayout<Vertex, Edge> layout = null;
-    try {
-      layout = new FRLayout<Vertex, Edge>(graphsetDec, graphSize);
-      layout.initialize();
-      layout.setRepulsionMultiplier(1.2);
-      while (!layout.done()) {
-        layout.step();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    timer2.end();
-
-    // Visualization code starts here
-    VisualizationViewer<Vertex, Edge> vv = new VisualizationViewer<>(layout);
-    vv.setPreferredSize(new Dimension(850, 850)); // Set the viewing area size
-    // vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
-    // vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
-
-    // Set up tooltips
-    vv.setVertexToolTipTransformer(vertex -> "Vertex Info: " + vertex.toString());
-    vv.setEdgeToolTipTransformer(edge -> "Edge Info: " + edge.toString());
-
-    DefaultModalGraphMouse<Vertex, Edge> graphMouse = new DefaultModalGraphMouse<>();
-    graphMouse.setMode(DefaultModalGraphMouse.Mode.TRANSFORMING);
-    vv.setGraphMouse(graphMouse);
-
-    JFrame frame = new JFrame("Graph Visualization");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(vv);
-    frame.pack();
-    frame.setVisible(true);
-
-    StringBuilder sb = new StringBuilder();
-    for (Vertex v : graphsetDec.getVertices()) {
-      Point2D p = layout.apply(v);
-      sb.append("\n" + v.toString() + "\n" + p.toString() + "\n");
-    }
-    report(sb.toString());
+    //JUNG LAYOUTS
+    graphsetDec.initFR();
+    graphsetDec.initFR2();
+    graphsetDec.initKK();
 
     print("stop");
   }
--- src/main/java/edu/ForceDrawnGraphs/jung/GraphsetDecorator.java
+++ src/main/java/edu/ForceDrawnGraphs/jung/GraphsetDecorator.java
@@ -1,19 +1,31 @@
 package edu.ForceDrawnGraphs.jung;
 
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Set;
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
 
 import edu.ForceDrawnGraphs.interfaces.Reportable;
 import edu.ForceDrawnGraphs.models.Edge;
 import edu.ForceDrawnGraphs.models.Graphset;
 import edu.ForceDrawnGraphs.models.Vertex;
+import edu.ForceDrawnGraphs.util.ProcessTimer;
 
 import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout2;
+import edu.uci.ics.jung.algorithms.layout.KKLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.ObservableGraph;
 
 public class GraphsetDecorator extends ObservableGraph<Vertex, Edge> implements Reportable {
   private final Graphset graphset;
+  private final Dimension graphSize = new Dimension(1600, 900);
+  private final FRLayout<Vertex, Edge> layout = new FRLayout<>(this, graphSize);
+  private final FRLayout2<Vertex, Edge> layout2 = new FRLayout2<>(this, graphSize);
+  private final KKLayout<Vertex, Edge> layout3 = new KKLayout<>(this);
 
   public GraphsetDecorator(Graphset graphset) {
     super(new DirectedSparseMultigraph<Vertex, Edge>());
@@ -27,12 +39,68 @@ public class GraphsetDecorator extends ObservableGraph<Vertex, Edge> implements 
       Optional<Pair<Vertex>> endpoints = graphset.getAssociatedVertices(edge);
 
       if (endpoints.isPresent()) {
+        //TODO - weighted edges
         addVertex(endpoints.get().getFirst());
         addVertex(endpoints.get().getSecond());
-        //TODO - This should be a weighted edge?
         addEdge(edge, endpoints.get());
       }
     }
+  }
+
+  public void initFR() {
+    ProcessTimer timer = new ProcessTimer("FRLayout()::");
+    try {
+      layout.initialize();
+      layout.setRepulsionMultiplier(0.75); //def 0.75
+      layout.setAttractionMultiplier(0.75); //def 0.75
+      layout.setMaxIterations(700); //def 700
+      while (!layout.done()) {
+        layout.step();
+      }
+    } catch (Exception e) {
+      report("initFR()::" + e.getMessage());
+    }
+    timer.end();
+  }
+
+  public void initFR2() {
+    ProcessTimer timer = new ProcessTimer("FRLayout2()::");
+    try {
+      layout2.initialize();
+      layout2.setRepulsionMultiplier(0.75); //def 0.75
+      layout2.setAttractionMultiplier(0.75); //def 0.75
+      layout2.setMaxIterations(700); //def 700
+      while (!layout2.done()) {
+        layout2.step();
+      }
+    } catch (Exception e) {
+      report("initFR()::" + e.getMessage());
+    }
+    timer.end();
+  }
+
+  public void initKK() {
+    ProcessTimer timer = new ProcessTimer("KKLayout()::");
+    try {
+      layout3.initialize();
+      layout3.setAdjustForGravity(true);
+      layout3.setMaxIterations(700); //def 700
+      while (!layout3.done()) {
+        layout3.step();
+      }
+    } catch (Exception e) {
+      report("initKK()::" + e.getMessage());
+    }
+    timer.end();
+  }
+
+  private HashMap<Vertex, Point2D> getLayoutCoords(Layout<Vertex, Edge> layout) {
+    HashMap<Vertex, Point2D> coords = new HashMap<>();
+    for (Vertex v : getVertices()) {
+      Point2D p = layout.apply(v);
+      coords.put(v, p);
+    }
+    return coords;
   }
 
 }
