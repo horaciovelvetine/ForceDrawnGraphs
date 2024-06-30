dfc9cdaefd3ab5eff90e57392356b2ddb5cfb2c1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-06-29 21:06:57 -0600

    adds tests and layout builds - ready for some additional testing

--- src/main/java/edu/ForceDrawnGraphs/commands/LayoutTests.java
+++ src/main/java/edu/ForceDrawnGraphs/commands/LayoutTests.java
@@ -1,17 +1,28 @@
 package edu.ForceDrawnGraphs.commands;
 
+import java.util.concurrent.CompletableFuture;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import edu.ForceDrawnGraphs.interfaces.Reportable;
+import edu.ForceDrawnGraphs.jung.GraphsetDecorator;
 import edu.ForceDrawnGraphs.util.FirstResponder;
 import edu.ForceDrawnGraphs.models.Graphset;
 
 @ShellComponent
 public class LayoutTests implements Reportable {
   private static final Graphset testset = FirstResponder.desrializeResponset();
+  private static final GraphsetDecorator graphsetDec = new GraphsetDecorator(testset);
 
   @ShellMethod("Run layout tests.")
   public void layout() {
+
+    //JUNG LAYOUTS
+    CompletableFuture<Void> initFRFuture = CompletableFuture.runAsync(() -> graphsetDec.initFR());
+    CompletableFuture<Void> initFR2Future = CompletableFuture.runAsync(() -> graphsetDec.initFR2());
+
+    CompletableFuture<Void> allFutures =
+        CompletableFuture.allOf(initFRFuture, initFR2Future);
+    allFutures.join();
     print("Running layout tests...");
   }
 }
